95be1b1b996e9b72c854665a62fc1be6471f206a
Hisho99 <s1136337@student.windesheim.nl>
2021-09-10 10:42:23 +0200

    added unit tests for Model

--- ControllerTest/Model_Competition_NextTrackShould.cs
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Model;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace ControllerTest
 {
@@ -26,6 +28,43 @@ namespace ControllerTest
             Assert.IsNull(_competition.NextTrack());
         }
 
+        [Test]
+        public void NextTrack_OneInQueue_ReturnTrack()
+        {
+            Track t = new Track("TestTrack",Hardcoded.Track);
+            _competition.Tracks.Enqueue(t);
+            Assert.AreEqual(_competition.Tracks.First(), _competition.NextTrack());
+        }
+
+        [Test]
+        public void NextTrack_OneInQueue_RemoveTrackFromQueue()
+        {
+            Track t = new Track("TestTrack", Hardcoded.Track);
+            _competition.Tracks.Enqueue(t);
+            _competition.Tracks.Enqueue(t);
+
+            var result = _competition.NextTrack();
+            result = _competition.NextTrack();
+            result = _competition.NextTrack();
+
+            Assert.IsNull(result);
+        }
+        
+        [Test]
+       public void NextTrack_TwoInQueue_ReturnNextTrack()
+        {
+            Track t1 = new Track("TestTrack1", Hardcoded.Track);
+            Track t2 = new Track("TestTrack2", Hardcoded.Track);
+            _competition.Tracks.Enqueue(t2);
+            _competition.Tracks.Enqueue(t1);
+
+
+            var first = _competition.Tracks.First();
+            var next = _competition.NextTrack();
+
+            Assert.AreEqual(first, next);
+
+        }
 
     }
 }
--- Model/Track.cs
+++ Model/Track.cs
@@ -21,5 +21,6 @@ public class Track
                 Sections.AddLast(section);
             }
         }
+
     }
 }
